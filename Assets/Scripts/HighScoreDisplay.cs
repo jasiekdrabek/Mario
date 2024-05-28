5eb763ef3382b2349f29cc8aba49c6c62f659901
Roman177-png <miron-2002@ukr.net>
2024-05-28 15:29:08 +0300

    highscore

--- Assets/Scripts/HighScoreDisplay.cs
+++ Assets/Scripts/HighScoreDisplay.cs
@@ -2,39 +2,6 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
-/*public class HighScoreDisplay : MonoBehaviour
-{
-    public TextMeshProUGUI[] highScoreTexts;
-    private HighScoreManager highScoreManager;
-
-    private void Start()
-    {
-        highScoreManager = FindObjectOfType<HighScoreManager>();
-        if (highScoreManager == null)
-        {
-            Debug.LogError("HighScoreDisplay: HighScoreManager not found!");
-        }
-    }
-
-    public void DisplayHighScores()
-    {
-        List<HighScoreEntry> highScores = highScoreManager.LoadHighScores();
-        Debug.Log("HighScoreDisplay: Displaying " + highScores.Count + " high scores.");
-
-        for (int i = 0; i < highScoreTexts.Length; i++)
-        {
-            if (i < highScores.Count)
-            {
-                highScoreTexts[i].text = $"Score: {highScores[i].score} Time: {highScores[i].time} Coins: {highScores[i].coins}";
-            }
-            else
-            {
-                highScoreTexts[i].text = "";
-            }
-        }
-    }
-}
-*/
 public class HighScoreDisplay : MonoBehaviour
 {
     public TextMeshProUGUI[] highScoreTexts;
